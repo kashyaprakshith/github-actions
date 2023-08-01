a14874977b65422d25ee998ce05670768f9c3d57
Rakshith Raghavendra Kashyap <58388399+kashyaprakshith@users.noreply.github.com>
2023-08-01 11:45:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami                                                                      = "ami-0c55b159cbfafe1f0"
+
+
+
+
+
+
+
+
   instance_type = "t2.micro"
 }
