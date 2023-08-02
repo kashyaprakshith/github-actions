b1be6bc1db01e96a603a6243d72e64c76ae31923
rakshith.kashyap <rakshith.kashyap@accenture.com>
2023-08-02 18:29:43 +0200

    added .prettierrc.json

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get("/hello", (req, res) => {
-  const name = req.query.name;
-  res.send(`<h1>Hello, ${name}</h1>`);
-});
+app.get('/hello', (req, res) => {
+  const name = req.query.name
+  res.send(`<h1>Hello, ${name}</h1>`)
+})
 
 app.get('/customerOnboarding', (req, res) => {
-  const name = req.query.name;
-  const uppercaseName = eval('"' + name + '"' + '.toUpperCase()');
-  res.send('Hi there, ' + uppercaseName);
-});
+  const name = req.query.name
+  const uppercaseName = eval('"' + name + '"' + '.toUpperCase()')
+  res.send('Hi there, ' + uppercaseName)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
