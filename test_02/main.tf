fabfc1834bd43cefd288e9c3f6c15209ee7bdf4c
Rakshith Raghavendra Kashyap <58388399+kashyaprakshith@users.noreply.github.com>
2023-08-14 11:11:01 +0200

    Update main.tf

--- test_02/main.tf
+++ test_02/main.tf
@@ -1,54 +1,39 @@
 terraform {
-  required_version = ">= 0.15.0"
+  required_version = ">= 1.1.0"
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_instance" "web" {
+ ^                                                                                                    ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
 }
 
-resource "aws_subnet" "subnet_a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-}
-
-resource "aws_subnet" "subnet_b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-}
-
-resource "aws_security_group" "sg" {
-  name_prefix = "example-"
-  
-  vpc_id = aws_vpc.main.id
+resource "aws_security_group" "web_sg" {
+  name = "web-sg"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
-resource "aws_instance" "example_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet_a.id
-  security_groups = [aws_security_group.sg.name]
-
-  tags = {
-    Name = "ExampleInstance"
-  }
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
