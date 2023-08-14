12b43264a2f9981d5d452856416a935753d13b0f
Rakshith Raghavendra Kashyap <58388399+kashyaprakshith@users.noreply.github.com>
2023-08-14 10:23:11 +0200

    Update main.tf

--- test/main.tf
+++ test/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI ID
+ ^                                                                                                    ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI ID
   instance_type = "t2.micro"
 
   tags = {
