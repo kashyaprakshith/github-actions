bcd647ff16e12be615a07412171c7373ee1b870b
Rakshith Raghavendra Kashyap <58388399+kashyaprakshith@users.noreply.github.com>
2023-08-14 11:05:21 +0200

    Update main.tf

--- test_02/main.tf
+++ test_02/main.tf
@@ -3,14 +3,52 @@ terraform {
 }
 
 provider "aws" {
-  region = "invalid-region-value"  # Intentional error here
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-example-bucket"
-  acl    = "private"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_security_group" "sg" {
+  name_prefix = "example-"
+  
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet_a.id
+  security_groups = [aws_security_group.sg.name]
 
   tags = {
-    Name = "ExampleBucket"
+    Name = "ExampleInstance"
   }
 }
