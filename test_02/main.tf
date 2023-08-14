5bf76466f94a405096f2a31b46f59c96a1c8a300
Rakshith Raghavendra Kashyap <58388399+kashyaprakshith@users.noreply.github.com>
2023-08-14 10:34:09 +0200

    Update main.tf

--- test_02/main.tf
+++ test_02/main.tf
@@ -1,17 +1,17 @@
 terraform {
-  required_version = ">= 0.15.0"  # Specify the required Terraform version
+  required_version = ">= 0.15.0"
   required_providers {
-    aws = ">= 3.0.0"  # Specify the required AWS provider version
+    aws = ">= 3.0.0"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"  # Change to your desired AWS region
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-example-bucket"  # Change to your desired bucket name
-  acl    = "private"            # Access control for the bucket (private, public-read, etc.)
+  bucket = "my-example-bucket"
+  acl    = "invalid_acl_value"  # Intentional error here
 
   tags = {
     Name = "ExampleBucket"
