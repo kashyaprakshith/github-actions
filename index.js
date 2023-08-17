3045fdcabe155d720cea9252b79ce054d73ddc99
rakshith.kashyap <rakshith.kashyap@accenture.com>
2023-08-17 07:08:21 +0200

    testing pre-commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
 app.get('/hello', (req, res) => {
-  const name = req.query.name;
-  res.send(`<h1>Hello, ${name}</h1>`);
-});
+  const name = req.query.name
+  res.send(`<h1>Hello, ${name}</h1>`)
+})
 
 app.get('/customerOnboarding', (req, res) => {
-  const name = req.query.name;
-  const uppercaseName = eval('"' + name + '"' + '.toUpperCase()');
-  res.send('Hi there, ' + uppercaseName);
-});
+                      const name = req.query.name
+                      const uppercaseName = eval('"' + name + '"' + '.toUpperCase()')
+  res.send('Hi there, ' + uppercaseName)
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+                  console.log(`Example app listening on port ${port}`)
+})
