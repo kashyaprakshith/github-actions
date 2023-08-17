f1b8b5baeaaf8f075ec61d1ea228315a6852af44
rakshith.kashyap <rakshith.kashyap@accenture.com>
2023-08-17 08:36:20 +0200

    testing

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-                           res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 app.get('/hello', (req, res) => {
-                           const name = req.query.name
-                            res.send(`<h1>Hello, ${name}</h1>`)
+                    const name = req.query.name
+                    res.send(`<h1>Hello, ${name}</h1>`)
 })
 
 app.get('/customerOnboarding', (req, res) => {
